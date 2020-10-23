6e96de58ab31f65e9be8f87fe79b4e1b39d288c7
Huy <huy1230191@gmail.com>
2020-10-23 10:14:00 +0700

    tinh tong

--- Source.cpp
+++ Source.cpp
@@ -4,11 +4,7 @@
 using namespace std;
 
 
-int hieu(int a, int b) {
-	int hieu = a - b;
 
-	return hieu;
-}
 
 int tong(int a, int b);
 int hieu(int a, int b);
@@ -16,14 +12,6 @@ int tich(int a, int b);
 float thuong(int a, int b);
 
 
-int tich(int a, int b);
-
-int tich(int a, int b)
-{
-	return a * b;
-
-}
-//Hieu
 
 int tong(int a, int b)
 {
@@ -32,9 +20,7 @@ int tong(int a, int b)
 	return s;
 }
 
-float thuong(int a, int b) {
-	return(a / b);
-}
+
 
 int main()
 {
@@ -48,18 +34,8 @@ int main()
 	cin >> a;
 	cout << "Nhap vao so b:";
 	cin >> b;
-
-	cout << "Hieu 2 so " << a << " va " << b << " la: " << hieu(a, b);
 	
-
 	cout << "Tong 2 so " << a << " va " << b << " la: " << tong(a, b);
-	cout << "Hieu 2 so " << a << " va " << b << " la: " << hieu(a, b);
-	cout << "Tich 2 so " << a << " va " << b << " la: " << tich(a, b);
-	cout << "Thuong 2 so " << a << " va " << b << " la: " << thuong(a, b);
-
-
-	
-
 	system("pause");
 	return 0;
 }
